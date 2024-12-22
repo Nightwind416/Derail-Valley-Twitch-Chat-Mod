d12d5df931af9733063cc92ffc67c87036293268
Christopher Lorr <christopher.e.lorr@gmail.com>
2024-12-22 12:20:27 -0600

    Add placeholder class for future GUI menu integration

---
+++ TwitchChatGui.cs
@@ -0,0 +1,10 @@
+namespace TwitchChat
+{
+    /// <summary>
+    /// Placeholder class for future GUI menu integration functionality
+    /// </summary>
+    public class GuiManager
+    {
+        // TODO: Implement GUI Manager
+    }
+}
